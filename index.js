493efad4f4a20dca30fcf7502da4c1e1db9d9f47
milon-hossain <abireshan32@gmial.com>
2024-02-24 03:00:46 +0600

    add js

--- index.js
+++ index.js
@@ -4,6 +4,25 @@ for(const btn of btns){
     btn.addEventListener('click',function(){
  const name=(event.target.parentNode.childNodes[1].innerText)
  const price=event.target.parentNode.childNodes[3].innerText
+ const catagory=event.target.parentNode.childNodes[5]
+ console.log(catagory)
  console.log(price)
+
+ const nameDIv2=document.getElementById('nameDiv');
+ const priceDIv2=document.getElementById('priceDiv');
+ const catagoryDIv2=document.getElementById('catagoryDiv');
+// const p=document.createElement('p');
+// p.innerText=name;
+// nameDIv2.appendChild(p);
+creatElement(nameDIv2,name)
+creatElement(priceDIv2,price)
+creatElement(catagoryDIv2,catagory)
     })
+}
+
+
+function creatElement(div,value){
+    const p=document.createElement('p');
+    p.innerText=value;
+    div.appendChild(p);
 }
